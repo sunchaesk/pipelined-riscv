b6d160096ca4e87c5ede7f47971653c454f33a59
Chris Kim <sunchaesk@gmail.com>
2024-07-11 16:28:45 -0400

    checked working branch instruction

--- test/tb_riscv.cpp
+++ test/tb_riscv.cpp
@@ -36,14 +36,22 @@ void dut_riscv_load_instruction(Vriscv *dut) {
     // dut->riscv__DOT__IF_unit__DOT__instr_mem[0] = 0x00702023; // sw x7, 0(x0)
 
     // dut->riscv__DOT__IF_unit__DOT__instr_mem[0] = 0x000003ef; // jal x7, 32
-    dut->riscv__DOT__IF_unit__DOT__instr_mem[0] = 0x010003ef; // jal x7, 16
+    // dut->riscv__DOT__IF_unit__DOT__instr_mem[0] = 0x010003ef; // jal x7, 16
+    // dut->riscv__DOT__IF_unit__DOT__instr_mem[1] = 0x003102b3; // add x5, x2, x3
+    // dut->riscv__DOT__IF_unit__DOT__instr_mem[2] = 0x00a20293; // addi x5, x4,10
+    // dut->riscv__DOT__IF_unit__DOT__instr_mem[3] = 0x00b20293; // addi x5, x4,11
+    // dut->riscv__DOT__IF_unit__DOT__instr_mem[4] = 0x02000093; // addi x1, x0, 32
+    // dut->riscv__DOT__IF_unit__DOT__instr_mem[5] = 0x03000093; // addi x1, x0, 32
+    // dut->riscv__DOT__IF_unit__DOT__instr_mem[6] = 0x04000093; // addi x1, x0, 32
+    // dut->riscv__DOT__IF_unit__DOT__instr_mem[7] = 0x05000093; // addi x1, x0, 32
+
+    ///// testing branch instr
+    dut->riscv__DOT__IF_unit__DOT__instr_mem[0] = 0x00310863; // beq x2, x3, 16
     dut->riscv__DOT__IF_unit__DOT__instr_mem[1] = 0x003102b3; // add x5, x2, x3
     dut->riscv__DOT__IF_unit__DOT__instr_mem[2] = 0x00a20293; // addi x5, x4,10
     dut->riscv__DOT__IF_unit__DOT__instr_mem[3] = 0x00b20293; // addi x5, x4,11
     dut->riscv__DOT__IF_unit__DOT__instr_mem[4] = 0x02000093; // addi x1, x0, 32
-    // dut->riscv__DOT__IF_unit__DOT__instr_mem[5] = 0x03000093; // addi x1, x0, 32
-    // dut->riscv__DOT__IF_unit__DOT__instr_mem[6] = 0x04000093; // addi x1, x0, 32
-    // dut->riscv__DOT__IF_unit__DOT__instr_mem[7] = 0x05000093; // addi x1, x0, 32
+
 
     // dut->riscv__DOT__IF_unit__DOT__instr_mem[0] = 0x008003ef; // beq x7, x0, 8
 
