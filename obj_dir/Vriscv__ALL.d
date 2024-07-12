a0dde092dbe0ebb1a59ba9d2fc654934074e7ac0
Chris Kim <sunchaesk@gmail.com>
2024-07-12 00:18:41 -0400

    cleaned repo

--- obj_dir/Vriscv__ALL.d
+++
@@ -1,7 +0,0 @@
-Vriscv__ALL.o: Vriscv__ALL.cpp Vriscv.cpp Vriscv.h \
- /usr/share/verilator/include/verilated_heavy.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vriscv__Syms.h \
- Vriscv__Trace.cpp /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated_trace.h Vriscv__Slow.cpp \
- Vriscv__Syms.cpp Vriscv__Trace__Slow.cpp
