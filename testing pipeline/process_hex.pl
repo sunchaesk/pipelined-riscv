0878d5d47c07cf8304911122fc5e7e01767d98c6
Michael Mo <michaelmoshui@gmail.com>
2024-07-12 17:03:01 -0400

    Show all lines

--- testing pipeline/process_hex.pl
+++ testing pipeline/process_hex.pl
@@ -21,7 +21,8 @@ my @lines = split /\n/, $hex_content;
 
 foreach my $line (@lines) {
     next if $line =~ /^\s*$/;  # Skip empty lines
-
+    print $line;
+    print "\n";
     # Remove leading hex address and trailing |...| sections
     $line =~ s/^\s*\w+\s+//; # Remove leading hex address
     $line =~ s/\s*\|.*$//;   # Remove trailing |...| section
@@ -30,13 +31,21 @@ foreach my $line (@lines) {
     my @line_list = split /\s+/, $line;
     my @line_list_1 = reverse @line_list[0..3];
     my @line_list_2 = reverse @line_list[4..7];
+    my @line_list_3 = reverse @line_list[8..11];
+    my @line_list_4 = reverse @line_list[12..15];
     
     my $line_1 = join "", @line_list_1;
     my $line_2 = join "", @line_list_2;
+    my $line_3 = join "", @line_list_3;
+    my $line_4 = join "", @line_list_4;
 
     print $fh $line_1;
     print $fh "\n";
     print $fh $line_2;
+    print $fh "\n";
+    print $fh $line_3;
+    print $fh "\n";
+    print $fh $line_4;
     print $fh "\n";
 }
 
