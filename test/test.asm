76f506b39530a2dc377d28f468b9115519965224
Chris Kim <sunchaesk@gmail.com>
2024-07-21 20:57:21 -0400

    some changes

--- test/test.asm
+++ test/test.asm
@@ -2,13 +2,13 @@
 .globl _start
 
 _start:
-    addi x12, x19, -2013
-    addi x6, x26, -1091
-    add x18, x18, x20
-    addi x0, x6, -1167
-    addi x12, x10, -208
-    addi x0, x28, 772
-    addi x23, x12, -1178
-    addi x30, x13, -1544
-    addi x15, x28, 529
-    addi x15, x18, -1549
+    addi x13, x26, -1001
+    addi x14, x21, -1593
+    sub x15, x0, x23
+    addi x31, x5, -866
+    sub x18, x8, x25
+    addi x27, x22, 1491
+    addi x19, x9, 1890
+    sub x24, x7, x25
+    sub x16, x8, x26
+    add x19, x11, x16
